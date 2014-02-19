b64725127083794bfec90db30f54834b0205264a
zabador <zabador2000@gmail.com>
2014-02-19 13:25:26 -0500

    changed api keys

--- maven/cse480poker/src/main/java/edu/oakland/testmavenagain/Ids.java
+++ maven/cse480poker/src/main/java/edu/oakland/testmavenagain/Ids.java
@@ -4,6 +4,6 @@ public class Ids {
 	public static final String WEB_CLIENT_ID =
 		"699958132030-a9n9sl6hj6ogj139u15hdn0ci002if2n.apps.googleusercontent.com";
 	public static final String ANDROID_CLIENT_ID =
-		"699958132030-g3oq5dbff8l8ncqluoc9sh0vef7jsaq9.apps.googleusercontent.com";
+		"699958132030-2gh2bscr6pmsb0fdnkve99knt51p2m9b.apps.googleusercontent.com";
 	public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
 }
