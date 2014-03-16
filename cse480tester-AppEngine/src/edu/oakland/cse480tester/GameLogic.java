8d024e18196cf78288fd97fd1313ce041cd051a3
cecile-le <cecile.le90@gmail.com>
2014-03-16 02:17:50 -0400

    Logic for n players

--- cse480tester-AppEngine/src/edu/oakland/cse480tester/GameLogic.java
+++ cse480tester-AppEngine/src/edu/oakland/cse480tester/GameLogic.java
@@ -1,130 +1,90 @@
 package edu.oakland.cse480tester;
-import java.util.Arrays;
-import java.util.Collections;
-
 
 public class GameLogic {
 
 	public static void main(String[] args){
-		
+		gameStart(7);
+	}
+	public static void gameStart(int n){
 		Deck deck = new Deck();
 		deck.shuffle();
 		
-		//Dealing first card
-		Card p1_card1 = deck.deal();
-		Card p2_card1 = deck.deal();
+		//Array of all players' hands
+		Hand [] playerHand = new Hand[n];	
+		for(int x=0; x<n; x++){
+			playerHand[x] = new Hand();
+		}
 		
-		//Dealing 2nd card
-		Card p1_card2 = deck.deal();
-		Card p2_card2 = deck.deal();
+		//Dealing 2 cards for each player
+		for (int i=0; i<2; i++){
+			for (int j=0; j<n; j++){
+				playerHand[j].addCard(deck.deal());
+			}
+		}
 		
-		//Discard a card
-		Card not_used_card1 = deck.deal();
+		for (int i=0; i<n; i++){
+			String prtCard = Integer.toString(i+1);
+			System.out.println("Player" + prtCard + "'s cards: " + playerHand[i]);
+		}
 		
-		//Dealing the flop 
-		Card flop1 = deck. deal();
-		Card flop2 = deck. deal();
-		Card flop3 = deck. deal();
+		System.out.print("\nFlop Turn River: ");
+		//Flop, Turn, and River cards
+		Card [] ftr = new Card[5];
+		for (int i=0; i<5; i++){
+			ftr[i] = deck.deal();
+			System.out.print( ftr[i] + " ");
+		}
+		System.out.println("\n");
 		
-		//Discard a card
-		Card not_used_card2 = deck.deal();
+		//Forming 7-card hand for each player
+		for (int i=0; i<n; i++){
+			for (int j=0; j<5; j++){
+				playerHand[i].addCard(ftr[j]);
+			}
+		}
 		
-		//Dealing the turn
-		Card turn = deck.deal();
+		for (int a=0; a<n; a++){
+			String prtCard = Integer.toString(a+1);
+			System.out.println("Player" + prtCard + "'s hand: " + playerHand[a]);
+		}
 		
-		//Discard a card
-		Card not_used_card3 = deck.deal();
+		HandEvaluator [] handEval = new HandEvaluator[n];	
+		for(int i=0; i<n; i++){
+			handEval[i] = new HandEvaluator();
+		}	
 		
-		//Dealing the river
-		Card river = deck.deal();
+		//Finding the best hand for each player
+		Hand [] bestHand = new Hand[n];
+		for (int i=0; i<n; i++){
+			bestHand[i] = handEval[i].getBest5CardHand(playerHand[i]);
+		}
 		
-		//Printing player 1 & 2 cards and community cards (for testing purpose)
-		System.out.println("Player 1 cards");
-		System.out.println(p1_card1.toString());
-		System.out.println(p1_card2.toString());
+		for (int a=0; a<n; a++){
+			String prtCard = Integer.toString(a+1);
+			System.out.println("\nPlayer " + prtCard + " best hand: " + bestHand[a]);
+			System.out.println(HandEvaluator.nameHand(bestHand[a]));
+		}
 		
-		System.out.println("\nPlayer 2 cards");
-		System.out.println(p2_card1.toString());
-		System.out.println(p2_card2.toString());
-		
-		System.out.println("\nFlop");
-		System.out.println(flop1.toString());
-		System.out.println(flop2.toString());
-		System.out.println(flop3.toString());
-		
-		System.out.println("\nTurn");
-		System.out.println(turn.toString());
-		
-		System.out.println("\nRiver");
-		System.out.println(river.toString());
-		
-		//Player 1 cards + community cards
-		Hand p1_7card_hand = new Hand();
-		p1_7card_hand.addCard(p1_card1);
-		p1_7card_hand.addCard(p1_card2);
-		p1_7card_hand.addCard(flop1);
-		p1_7card_hand.addCard(flop2);
-		p1_7card_hand.addCard(flop3);
-		p1_7card_hand.addCard(turn);
-		p1_7card_hand.addCard(river);
-		
-		System.out.println("\nPlayer 1's 7 cards");
-		System.out.println(p1_7card_hand.toString());
-		
-		//Player 2 cards + community cards
-		Hand p2_7card_hand = new Hand();
-		p2_7card_hand.addCard(p2_card1);
-		p2_7card_hand.addCard(p2_card2);
-		p2_7card_hand.addCard(flop1);
-		p2_7card_hand.addCard(flop2);
-		p2_7card_hand.addCard(flop3);
-		p2_7card_hand.addCard(turn);
-		p2_7card_hand.addCard(river);
-		
-		System.out.println("\nPlayer 2's 7 cards");
-		System.out.println(p2_7card_hand.toString());
-		
-		
-		//Player1's best 5-card hand
-		HandEvaluator p1_hand_eval = new HandEvaluator();
-		Hand p1_best_hand = new Hand();
-		p1_best_hand = p1_hand_eval.getBest5CardHand(p1_7card_hand);
-		
-		System.out.println("\nPlayer 1's best hand");
-		//Name of the best hand
-		System.out.println(HandEvaluator.nameHand(p1_best_hand));
-		System.out.println(p1_best_hand);
-		
-		
-		//Player2's best 5-card hand
-		HandEvaluator p2_hand_eval = new HandEvaluator();
-		Hand p2_best_hand = new Hand();
-		p2_best_hand = p2_hand_eval.getBest5CardHand(p2_7card_hand);
-		
-		System.out.println("\nPlayer 2's best hand");
-		//Name of the best hand
-		System.out.println(HandEvaluator.nameHand(p2_best_hand));
-		System.out.println(p2_best_hand);	
-		
-		
-		//Comparing player 1's and player 2's best hand
-		int compare = HandEvaluator.compareHands(p1_best_hand, p2_best_hand);
+		//Determining the winning hand
 		Hand winning_hand = new Hand();
-		if (compare == 1){
-			winning_hand = p1_best_hand;
-			System.out.println("\nPlayer 1 wins!");
-			//System.out.println(winning_hand);
+		winning_hand = playerHand[0];
+		for (int i=0; i<n-1; i++){
+			int compare = HandEvaluator.compareHands(winning_hand, playerHand[i+1]);
+			if (compare == 1){
+			//do nothing	
+			}
+			
+			if (compare == 2){
+				winning_hand = playerHand[i+1];
+			}
+			
+			if (compare == 0){
+			// do nothing
+			}
 		}
 		
-		if (compare == 2){
-			winning_hand = p2_best_hand;
-			System.out.println("\nPlayer 2 wins!");
-			//System.out.println(winning_hand);
-		}
-		
-		if (compare == 0){
-			winning_hand = p1_best_hand;
-			System.out.println("\nTie!");
+		System.out.println("\nWinning hand: " + winning_hand);
+		System.out.println(HandEvaluator.nameHand(winning_hand));
+
 		}
 	}
-}
