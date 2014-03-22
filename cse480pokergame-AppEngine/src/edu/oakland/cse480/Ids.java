afebb22f788cd9f0fa990c1dfda30d9497b681bd
Geoff Misiak <gomisiak@oakland.edu>
2014-03-22 13:19:02 -0400

    fixed some merge issues

--- cse480pokergame-AppEngine/src/edu/oakland/cse480/Ids.java
+++
@@ -1,7 +0,0 @@
-package edu.oakland.cse480;
-
-public class Ids {
-	public static final String WEB_CLIENT_ID = "442435041673.apps.googleusercontent.com";
-	public static final String ANDROID_CLIENT_ID = "442435041673-uijg55tjq7codh2krn3nub4t9.apps.googleusercontent.com";
-	public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
-}
